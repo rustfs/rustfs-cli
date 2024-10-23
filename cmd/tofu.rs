3b96db633b4a09b60e1e6f2aebf1b46d404af846
laoliu <lygn128@163.com>
2024-10-23 17:48:15 +0800

    use minio client

--- cmd/tofu.rs
+++ cmd/tofu.rs
@@ -1,6 +1,8 @@
+use aws_sdk_s3::waiters::bucket_exists;
 //use aws_sdk_s3::{Client, Config};
 //use aws_sdk_s3::config::Credentials;
-use aws_sdk_s3::{Client, Config};
+use aws_sdk_s3::{config::endpoint, Client, Config};
+use minio::s3::{client};
 use aws_sdk_s3::config::Credentials;
 use rand::{distributions::Alphanumeric, Rng};
 use tokio::runtime::Runtime;
@@ -8,6 +10,13 @@ use std::error::Error;
 use std::thread::sleep;
 use tokio;
 use aws_sdk_s3::config::Region;
+
+use minio::s3::args::{BucketExistsArgs, MakeBucketArgs};
+//use minio::s3::builders::ObjectContent;
+use minio::s3::client::ClientBuilder;
+use minio::s3::creds::StaticProvider;
+use minio::s3::http::BaseUrl;
+use std::path::Path;
 
 //use aws_config::meta::region::RegionProviderChain;
 use std::{fs::File, io::Write, path::PathBuf, process::exit};
@@ -33,20 +42,40 @@ pub fn check_bucket_permissions(
     region: &str,
 ) -> Result<bool, Box<dyn Error>> {
     // 创建 AWS 配置和 S3 客户端
-    println!("Generated random bucket name:");
-    let credentials = Credentials::new(access_key, secret_key, None, None, "my-credentials");
+    println!("Generated random bucket name:{}", url);
+    let credentials = Credentials::new(access_key, secret_key, None, None, "");
+    //let credentials = Credentials::;
     let region = Region::new(region.to_string());
 
     let config = Config::builder()
         .region(region)
         .endpoint_url(url)
+        .behavior_version(aws_sdk_s3::config::BehaviorVersion::v2023_11_09())
         .credentials_provider(credentials)
-        .behavior_version_latest()
         .build();
-    let s3_client = Client::from_conf(config);
+    dbg!(config.clone());
+    //let s3_client = Client::from_conf(config);
+
+    let static_provider = StaticProvider::new(
+        access_key,
+        secret_key,
+        None,
+    );
+
+    let base_url = url.parse::<BaseUrl>()?;
+    let s3_client = ClientBuilder::new(base_url.clone())
+        .provider(Some(Box::new(static_provider)))
+        .build()?;
+
 
     // 生成一个随机的 bucket 名字
     let random_bucket_name = generate_random_bucket_name();
+    let bucket_exists_options =  BucketExistsArgs{
+        region:None,
+        bucket: &random_bucket_name,
+        extra_headers: None,
+        extra_query_params: None,
+    };
     println!("Generated random bucket name: {}", random_bucket_name);
 
     // 检查 bucket 权限
@@ -57,12 +86,7 @@ pub fn check_bucket_permissions(
         .enable_all() // 启用所有 Tokio 的功能
         .build()?; // 构建运行时
 
-    // 使用运行时来执行异步函数，并等待其完成
-    //rt.block_on(s3_client.head_bucket().bucket(&random_bucket_name).send().await)?;
-    let head_bucket_result = rt.block_on(s3_client.head_bucket().bucket(&random_bucket_name).send());
-    //tokio::time::sleep(std::time::Duration::from_secs(200)).await;
-    sleep(std::time::Duration::from_secs(20));
-    println!("--------------------");
+    let head_bucket_result = rt.block_on(s3_client.bucket_exists(&bucket_exists_options));
     match head_bucket_result {
         Ok(_) => {
             println!("Bucket '{}' exists and is accessible.", random_bucket_name);
@@ -70,7 +94,8 @@ pub fn check_bucket_permissions(
         }
         Err(err) => {
             let err_msg = format!("{:?}", err);
-            if err_msg.contains("NotFound") {
+            println!("err: {}\n", err_msg);
+            if err_msg.contains("InvalidBucketName") {
                 println!("Bucket '{}' does not exist, access key and secret key are valid.", random_bucket_name);
                 Ok(true)
             } else if err_msg.contains("AccessDenied") {
